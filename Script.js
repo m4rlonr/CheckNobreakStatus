6522e868e715a6b2c104c3e56d56f423e2c67de7
m4rlonr <marlon.rodrigues14@outlook.com>
2024-04-05 16:33:13 -0400

    add bot telegram

--- Script.js
+++ Script.js
@@ -2,6 +2,9 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 require("dotenv").config();
 
+const TelegramBot = require("node-telegram-bot-api");
+const bot = new TelegramBot(process.env.BOTTOKEN, { polling: true });
+
 // URL que você deseja fazer a requisição
 const statusUrl = process.env.BaseUrl1;
 const bateryUrl = process.env.BaseUrl2;
@@ -57,6 +60,19 @@ async function fetchStatus() {
   } catch (error) {
     console.error("[ERRO] - Rquisição falhou.\n", error);
   }
+
+  // Functions
+  if (nobreakBatery < 80 && (lastState == 10 || lastState == 32)) {
+    try {
+      await axios.post(
+        `http://${process.env.ESPIP}/setpoint?batery=${nobreakBatery}`
+      );
+    } catch (error) {
+      console.log(Error);
+    }
+    SendMessagemBot(data);
+  }
+
   if (lastState == 10 || lastState == 32) {
     console.log(
       `\n[STATUS: ${nobreakStatus}] - Falha na entrada\n[INFO] - Bateria a ${nobreakBatery}%\nHora local: ${data.getHours()}h ${data.getMinutes()}m ${data.getSeconds()}s`
@@ -72,6 +88,8 @@ async function fetchStatus() {
       console.log(error);
     }
   }
+
+  // Funcao de mudanca de estado
   if (lastState !== nobreakStatus) {
     try {
       await axios.post(
@@ -117,9 +135,35 @@ async function fetchStatus() {
         );
         break;
     }
+    SendMessagemBot(data);
+  }
+}
+
+// Funcao que manda mensagem com o bot
+async function SendMessagemBot(data) {
+  try {
+    var message = "";
+    switch (nobreakStatus) {
+      case 10:
+        message = `\n[STATUS: ${nobreakStatus}] - Falha na entrada\n[INFO] - Bateria a ${nobreakBatery}%\nHora local: ${data.getHours()}h ${data.getMinutes()}m ${data.getSeconds()}s`;
+        break;
+      case 11:
+        message = `\n[STATUS: ${nobreakStatus}] - Entrada normalizada\n[INFO] - Bateria a ${nobreakBatery}%\nHora local: ${data.getHours()}h ${data.getMinutes()}m ${data.getSeconds()}s`;
+        break;
+      case 32:
+        message = `\n[STATUS: ${nobreakStatus}] - Aguardando energia\n[INFO] - Bateria a ${nobreakBatery}%\nHora local: ${data.getHours()}h ${data.getMinutes()}m ${data.getSeconds()}s`;
+        break;
+      case 33:
+        message = `\n[STATUS: ${nobreakStatus}] - Energia reestabelecida\n[INFO] - Bateria a ${nobreakBatery}%\nHora local: ${data.getHours()}h ${data.getMinutes()}m ${data.getSeconds()}s`;
+        break;
+    }
+
+    bot.sendMessage(process.env.CHATID, message);
+  } catch (error) {
+    console.error("Erro ao enviar a mensagem:", error);
   }
 }
 
 setInterval(() => {
   fetchStatus();
-}, 1000);
+}, 6000);
